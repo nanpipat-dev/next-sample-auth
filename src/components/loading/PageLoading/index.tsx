2a41029aee2ec07943e667e95bc84e750ae7c1e0
nanpipat <nanpipat.k.dev@gmail.com>
2022-01-29 23:38:33 +0700

    [:zap:] add loading

--- src/components/loading/PageLoading/index.tsx
+++ src/components/loading/PageLoading/index.tsx
@@ -3,10 +3,13 @@ import classNames from 'classnames'
 function PageLoading(): JSX.Element {
   return (
 
+    // <div  className={styles['container-loading']}>
+    //   <span className={classNames(styles['loader__ball'], styles['loader__ball--1'])}></span>
+    //   <span className={classNames(styles['loader__ball'], styles['loader__ball--2'])}></span>
+    //   <span className={classNames(styles['loader__ball'], styles['loader__ball--3'])}></span>
+    // </div>
     <div  className={styles['container-loading']}>
-      <span className={classNames(styles['loader__ball'], styles['loader__ball--1'])}></span>
-      <span className={classNames(styles['loader__ball'], styles['loader__ball--2'])}></span>
-      <span className={classNames(styles['loader__ball'], styles['loader__ball--3'])}></span>
+      <div className={styles['loading-pulse']}></div>
     </div>
   )
 }
